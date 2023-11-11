2c1cea1121e16f24dbde5fcfd688e309d6c83f96
avenseq <147945651+avenseq@users.noreply.github.com>
2023-11-11 14:31:51 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+
+import handlers
+from config_reader import config
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.bot_token.get_secret_value())
+# Диспетчер
+dp = Dispatcher()
+
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    # Включаем логирование, чтобы не пропустить важные сообщения
+    logging.basicConfig(level=logging.INFO)
+    # Объект бота
+    bot = Bot(token=config.bot_token.get_secret_value())
+    # Диспетчер
+    dp = Dispatcher()
+    dp.include_router(handlers.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
